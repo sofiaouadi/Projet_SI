f9f13970bdc5c5b781e80f027b833e7bb6de340b
sofiaouadi <133815006+sofiaouadi@users.noreply.github.com>
2024-12-29 15:26:18 +0100

    Add files via upload

--- recrutement/models.py
+++ recrutement/models.py
@@ -1,1 +1,9 @@
+from django.db import models
 
+# Create your models here.
+from personnel.models import Personnel
+
+class recrutement(models.Model):
+ dateRec= models.DateField
+ posteRecru = models.CharField
+ statutrecrutement = models.CharField
