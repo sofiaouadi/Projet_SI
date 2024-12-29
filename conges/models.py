2ad9c77247f6657d8a0b26c89136ae4174ba31da
sofiaouadi <133815006+sofiaouadi@users.noreply.github.com>
2024-12-29 15:21:54 +0100

    Add files via upload

--- conges/models.py
+++ conges/models.py
@@ -1,1 +1,10 @@
+from django.db import models
 
+# Create your models here.
+from personnel.models import Personnel
+
+class conges(models.Model):
+ type= models.CharField(max_length=50)
+ dateD= models.DateField
+ dateF= models.DateField
+ Empconge = models.ForeignKey(Personnel, on_delete=models.CASCADE)
