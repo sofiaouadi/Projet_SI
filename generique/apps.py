e430fc60b08b9c851a886b9455f4afcc1842c994
sofiaouadi <133815006+sofiaouadi@users.noreply.github.com>
2024-12-29 15:24:24 +0100

    Add files via upload

---
+++ generique/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GeneriqueConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'generique'
--- generique/models.py
+++ generique/models.py
@@ -1,1 +1,13 @@
+from django.db import models
 
+# Create your models here.
+from django.contrib.auth.models import User
+
+class Favori(models.Model):
+    utilisateur = models.ForeignKey(User, on_delete=models.CASCADE, related_name="favoris")
+    nom = models.CharField(max_length=100)  # Nom de la fonctionnalité
+    url = models.CharField(max_length=255)  # URL de la fonctionnalité
+    date_ajout = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.utilisateur.username} - {self.nom}"
