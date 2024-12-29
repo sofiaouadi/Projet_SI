20f568868dbabc258f61f516923ec723966d876d
sofiaouadi <133815006+sofiaouadi@users.noreply.github.com>
2024-12-29 15:22:51 +0100

    Add files via upload

--- contrat/models.py
+++ contrat/models.py
@@ -1,1 +1,11 @@
+from django.db import models
 
+# Create your models here.
+from personnel.models import Personnel
+
+class Contrat(models.Model):
+ type= models.CharField(max_length=3)
+ dateD= models.DateField
+ dateF= models.DateField
+ salaire= models.FloatField(max_length=12) 
+ contratEmp= models.ForeignKey(Personnel, on_delete=models.CASCADE)
