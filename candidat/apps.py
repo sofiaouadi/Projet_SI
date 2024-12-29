781a5e48e43f702b2f36cee6186a533d19a7c99d
sofiaouadi <133815006+sofiaouadi@users.noreply.github.com>
2024-12-29 15:20:58 +0100

    Add files via upload

---
+++ candidat/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CandidatConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'candidat'
--- candidat/models.py
+++ candidat/models.py
@@ -1,1 +1,11 @@
+from django.db import models
 
+# Create your models here.
+from recrutement.models import recrutement
+class candidat(models.Model):
+ nom= models.CharField(max_length=50)
+ prenom= models.CharField(max_length=50)
+ numT= models.FloatField(max_length=12) 
+ emeil = models.CharField
+ statutCondidat = models.CharField
+ recrutementCandidat = models.ForeignKey(recrutement, on_delete=models.CASCADE)
