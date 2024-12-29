697fc342ed5e64b5e91d3b202cda42d98edd2b04
sofiaouadi <133815006+sofiaouadi@users.noreply.github.com>
2024-12-29 15:23:39 +0100

    Add files via upload

---
+++ evaluation/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Evaluation  
+admin.site.register(Evaluation)
--- evaluation/models.py
+++ evaluation/models.py
@@ -1,1 +1,9 @@
+from django.db import models
 
+# Create your models here.
+from personnel.models import Personnel
+
+class Evaluation(models.Model):
+ dateEv= models.DateField
+ score= models.FloatField(max_length=6) 
+ commentaire= models.CharField(max_length=300)
