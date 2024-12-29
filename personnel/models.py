7d78de6afde91fb86f5abcc7ba2d020c5b65863b
sofiaouadi <133815006+sofiaouadi@users.noreply.github.com>
2024-12-29 15:25:19 +0100

    Add files via upload

--- personnel/models.py
+++ personnel/models.py
@@ -1,1 +1,17 @@
+from django.db import models
 
+# Create your models here.
+from service.models import service
+#from recrutement.models import recrutement
+
+class Personnel(models.Model):
+ nom= models.CharField(max_length=50)
+ prenom= models.CharField(max_length=50)
+ dateN= models.DateField
+ numT= models.FloatField(max_length=12) 
+ email = models.CharField
+ posteOccupe = models.CharField(max_length=50)
+ dateEmbauche = models.DateField
+ adresse = models.TextField 
+ serviceEmp = models.ForeignKey(service, on_delete=models.CASCADE) 
+ #Embauchement = models.ForeignKey(recrutement.recrutement, on_delete=models.CASCADE)
